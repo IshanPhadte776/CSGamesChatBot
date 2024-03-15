6926e21e4c2db4a68c7f54fd9c889b68d6e230d8
Ishan Phadte <ishanphadte@gmail.com>
2024-03-15 19:38:41 -0400

    Added the sendMessages to Backend Js function

--- index.js
+++ index.js
@@ -2,8 +2,22 @@ const chatForm = get('form');
 const chatInput = get('input');
 const chatBox = get('main');
 
-appendMessage('bot', 'This is a bot bubble');
-appendMessage('user', 'This is a user bubble');
+let lastUserQuery;
+let messageLimit = 5;
+const previousMessages = [];
+
+appendMessage('bot', 'Hello');
+appendMessage('bot', 'How can we aid you?');
+appendMessage('bot', 'How can we aid you?');
+appendMessage('bot', 'How can we aid you?');
+appendMessage('bot', 'How can we aid you?');
+appendMessage('bot', 'How can we aid you?');
+appendMessage('user', 'I have a runny nose');
+appendMessage('user', 'I have a bloody nose');
+
+
+sendMessagesAndContextToModel();
+
 
 chatForm.addEventListener('submit', event => {
   event.preventDefault();
@@ -21,9 +35,37 @@ function appendMessage(side, text) {
     </div>`;
   chatBox.insertAdjacentHTML('beforeend', bubble);
   chatBox.scrollTop += 500;
+
+  if (side == 'user') {
+    lastUserQuery = text
+  }
+
+  previousMessages.push(text);
+
 }
+
+
 
 // Utils
 function get(selector, root = document) {
   return root.querySelector(selector);
+}
+
+function sendMessagesAndContextToModel () {
+  let buf = []
+  let count = 0 
+  for (var i = previousMessages.length - 1; i > previousMessages.length - messageLimit - 1; i--) {
+    buf[count] = previousMessages[i]
+    count += 1
+  }
+
+
+  const messages = {
+    "query": lastUserQuery,
+    "buf": buf
+  }
+
+  console.log(messages)
+
+  return messages
 }
